dccf0f5fab3efe60137f518e68f5a0213fb2ca93
Thomas <tg.59290@gmail.com>
2019-10-24 16:39:46 +0200

    add data to csv file i in index view and display table in template

--- views.py
+++ views.py
@@ -1,11 +1,54 @@
+import pandas as pd
+import re
+
 from flask import Flask
 from flask import render_template
+
+from data.geographical_information import states_location
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    data = pd.read_csv("data/median_income_data.csv", decimal=",")
+    # get all columns names, clean it and rename the dataframe
+    columns = list(data)
+    columns = [value.lower().replace(" ", "_") for value in columns]
+    data.columns = columns
+
+    #Turn all data in pure integers
+    for column in columns:
+        if column != "state":
+            test = list(data[column])
+            for index, value in enumerate(test):
+                value = re.sub('[^0-9]', "", value)
+                try:
+                    value = int(value)
+                    test[index] = value
+                except Exception as e:
+                    test[index] = None
+            data[column] = test
+
+    # Add data about richest and poorest race and difference between poorest and richest
+    races_values = data[["white", "asian", "black_or_african_american", "american_indian_and_alaska_native", "native_hawaiian_and_other_pacific_islander", "some_other_race", "two_or_more_races"]]
+    data["richest_race"] = races_values.idxmax(axis=1)
+    data["richest"] = races_values.max(axis=1)
+    data["poorest_race"] = races_values.idxmin(axis=1)
+    data["poorest"] = races_values.min(axis=1)
+    data["difference"] = data["richest"] - data["poorest"]
+
+    # Add géographical data about where the state is located in the US
+    location = []
+    for state in data["state"]:
+        if state in states_location["northern"]:
+            location.append("northern")
+        elif state in states_location["southern"]:
+            location.append("southern")
+        else:
+            location.append("western")
+    data["location"] = location
+
+    return render_template("index.html", data_table=data.to_html())
 
 @app.route('/analysis')
 def analysis():
