8f931cb28c5cdeb8b1b2d19f9574c5b17b766105
Thomas <tg.59290@gmail.com>
2019-10-24 14:13:23 +0200

    base template to display datat

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Let's analyse some data !"
+    return render_template("index.html", name="test")
