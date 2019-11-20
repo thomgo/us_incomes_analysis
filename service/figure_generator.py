ec8f9a3c3a8edc0a3582b50b1ec459b8e0156d54
Thomas <tg.59290@gmail.com>
2019-11-20 15:30:26 +0100

    mooved analys logic from view to figure generator service

---
+++ service/figure_generator.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class FigureGenerator():
+    """Produce figures needed for analysis"""
+
+    data = pd.read_csv("data/final_income_data.csv", decimal=",")
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def generate_richest_poorest_chart_by_race(cls):
+        # Piechart with frequence of races in % in richest_race column
+        cls.data["richest_race"].value_counts(normalize=True).plot(kind='pie', autopct='%1.1f%%', title="Races with highest income")
+        plt.savefig(fname="static/images/distri_richest_race")
+        plt.close()
+        # Piechart with frequence of races in % in poorest_race column
+        cls.data["poorest_race"].value_counts(normalize=True).plot(kind='pie', autopct='%1.1f%%', title="races with lowest income")
+        plt.savefig(fname="static/images/distri_poorest_race")
+        plt.close()
+
+    @classmethod
+    def generate_incomes_hist_by_race(cls):
+        # Keep only the incomes by race
+        subset = cls.data[["white", "asian", "black", "indian", "pacific", "other", "two_or_more_races"]]
+        # Turn pacific column to float, if not mean and median does not work
+        subset["pacific"] = subset["pacific"].astype(float)
+        # Create one histogram by race for incomes
+        for label, column in subset.iteritems():
+            mean = int(round(column.mean(skipna=True)))
+            median = int(round(column.median(skipna=True)))
+            title = "Histrogram of {} incomes".format(label)
+            text = "mean : {}  median : {}".format(mean, median)
+            fname = "static/images/hist_{}".format(label)
+            column.plot(kind='hist', title=title)
+            plt.figtext(x=0.2, y=0.01, s=text)
+            plt.savefig(fname=fname)
+            plt.close()
--- views.py
+++ views.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import render_template
 from data.geographical_information import states_location
 from service.data_manipulator import DataManipulator as dm
+from service.figure_generator import FigureGenerator as fg
 
 app = Flask(__name__)
 # Avoid runtime and threading errors because not displaying the charts
@@ -27,31 +28,9 @@ def index():
 @app.route('/analysis')
 def analysis():
     """Create and save as images all the figures needed for the analysis"""
-    if path.exists("data/final_income_data.csv"):
-        data = pd.read_csv("data/final_income_data.csv", decimal=",")
-        # Piechart with frequence of races in % in richest_race column
-        data["richest_race"].value_counts(normalize=True).plot(kind='pie', autopct='%1.1f%%', title="Races with highest income")
-        plt.savefig(fname="static/images/distri_richest_race")
-        plt.close()
-        # Piechart with frequence of races in % in poorest_race column
-        data["poorest_race"].value_counts(normalize=True).plot(kind='pie', autopct='%1.1f%%', title="races with lowest income")
-        plt.savefig(fname="static/images/distri_poorest_race")
-        plt.close()
-
-        # Keep only the incomes by race
-        subset = data[["white", "asian", "black", "indian", "pacific", "other", "two_or_more_races"]]
-        # Turn pacific column to float, if not mean and median does not work
-        subset["pacific"] = subset["pacific"].astype(float)
-        # Create one histogram by race for incomes
-        for label, column in subset.iteritems():
-            mean = int(round(column.mean(skipna=True)))
-            median = int(round(column.median(skipna=True)))
-            title = "Histrogram of {} incomes".format(label)
-            text = "mean : {}  median : {}".format(mean, median)
-            fname = "static/images/hist_{}".format(label)
-            column.plot(kind='hist', title=title)
-            plt.figtext(x=0.2, y=0.01, s=text)
-            plt.savefig(fname=fname)
-            plt.close()
+    if not path.exists("data/final_income_data.csv"):
+        dm.generate_improved_data()
+    fg.generate_richest_poorest_chart_by_race()
+    fg.generate_incomes_hist_by_race()
 
     return render_template("analysis.html")
