438dc8da41a14fc7b434d1673afa57754bde6826
Thomas <tg.59290@gmail.com>
2019-11-20 14:44:45 +0100

    split generate improved data in smaller function for each action

--- service/data_manipulator.py
+++ service/data_manipulator.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from data.geographical_information import states_location
 
+
 class DataManipulator():
     """Make all operation on the application data
     All the views logic to be lighhter"""
@@ -16,21 +17,12 @@ class DataManipulator():
 
     @classmethod
     def generate_improved_data(cls):
+        """read data, call methods to keep only numbers,
+        add information about location, inequalities and save the file """
         data = pd.read_csv(DataManipulator.data_file, decimal=",")
         # Rename columns with shorter names
         data.columns = ["state", "white", "error1", "black", "error2", "indian", "error3", "asian", "error4", "pacific", "error5", "other", "error6", "two_or_more_races", "error7" ]
-
-        # Clean strings and turn all data in pure integers
-        for column in data.columns[1:]:
-            column_values = list(data[column])
-            for index, value in enumerate(column_values):
-                # Keep only integer value from the string
-                try:
-                    value = int(re.sub('[^0-9]', "", value))
-                    column_values[index] = value
-                except ValueError:
-                    column_values[index] = None
-            data[column] = column_values
+        data = cls.keep_numbers_only(data)
 
         # Keep only data related to incomes by race
         races_values = data[["white", "asian", "black", "indian", "pacific", "other", "two_or_more_races"]]
@@ -57,3 +49,26 @@ class DataManipulator():
 
         # Save the improved data as a csv file
         data.to_csv(path_or_buf="data/final_income_data.csv", index=False)
+
+    @staticmethod
+    def keep_numbers_only(data):
+        """Clean strings and turn all data in pure integers"""
+        for column in data.columns[1:]:
+            column_values = list(data[column])
+            for index, value in enumerate(column_values):
+                # Keep only integer value from the string
+                try:
+                    value = int(re.sub('[^0-9]', "", value))
+                    column_values[index] = value
+                except ValueError:
+                    column_values[index] = None
+            data[column] = column_values
+        return data
+
+    # @staticmethod
+    # def keep_numbers_only(cls):
+    #     pass
+    #
+    # @staticmethod
+    # def keep_numbers_only(cls):
+    #     pass
